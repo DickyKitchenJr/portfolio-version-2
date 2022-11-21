03356f14d0e4540cd56e7dd4d8f89925fdb24a92
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-11-21 16:32:29 -0500

    adjusted styling; began AboutMe section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,24 @@
-import { Parallax } from 'react-parallax';
-import code from './Images/codeBackground.jpg'
-import Header from './Components/Header';
-import Skills from './Components/Skills';
-import './App.css';
+import { Parallax } from "react-parallax";
+import code from "./Images/codeBackground.jpg";
+import Header from "./Components/Header";
+import Skills from "./Components/Skills";
+import AboutMe from "./Components/AboutMe";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Parallax
+      <Parallax
         blur={2}
         bgImage={code}
         bgImageAlt="various lines of code"
         contentClassName="main"
         strength={600}
-      ><Header />
-    <Skills /></Parallax>
-    
+      >
+        <Header />
+        <Skills />
+        <AboutMe />
+      </Parallax>
     </>
   );
 }
---
+++ portfolio/src/Components/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./AboutMe.css"
+
+function AboutMe() {
+  return (
+    <>
+      <div className='aboutMeTitle'>
+        <h2>About Me</h2>
+      </div>
+    </>
+  )
+}
+
+export default AboutMe

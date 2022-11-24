3e1c9115f2788c6b70893bb3ecee243be8a3435d
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-11-24 13:07:27 -0500

    adjusted styling

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
       </div>
       <div className="contactBackground">
         <div className="contact">
-          <form action="https://formsubmit.co/your@email.com" method="POST">
+          <form action="https://formsubmit.co/dkitchencodes@gmail.com" method="POST">
             <p>Have a job or a question for me?</p>
             <input type="text" name="name" placeholder="Your Name" required />
             <input

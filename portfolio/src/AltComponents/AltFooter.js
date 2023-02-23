d4bace08599e08eb49ba81a6a4d9f7d8b2fe9466
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-23 14:01:49 -0500

    Create AltFooter.js

---
+++ portfolio/src/AltComponents/AltFooter.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const GitHub = <FontAwesomeIcon icon={faGithub} />;
+const LinkedIn = <FontAwesomeIcon icon={faLinkedin} />;
+const Instagram = <FontAwesomeIcon icon={faInstagram} />;
+
+
+function AltFooter() {
+  return (
+    <>
+    <p className="altFooter">
+      Find me on &nbsp;&nbsp;
+      <a
+        href="https://github.com/DickyKitchenJr"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {GitHub} GitHub
+      </a>
+      ,&nbsp;&nbsp;
+      <a
+        href="https://www.linkedin.com/in/dicky-kitchen-jr-89727b240/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {LinkedIn} LinkedIn
+      </a>
+      , and &nbsp;&nbsp;
+      <a
+        href="https://www.instagram.com/oldguylearnscoding/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {Instagram} Instagram
+      </a>
+    </p>
+    </>
+  )
+}
+
+export default AltFooter

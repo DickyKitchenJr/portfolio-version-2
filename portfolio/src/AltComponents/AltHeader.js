5ebf6e072f96f02636d56e4603a9fa0f4afcffb1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-23 14:01:52 -0500

    Create AltHeader.js

---
+++ portfolio/src/AltComponents/AltHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AltHeader() {
+  return (
+    <>
+      <h1 className="altBanner">DK's Portfolio</h1>
+      <div className="altHero">
+        <p className="altBanner">Coding Makes Me Happy</p>
+      </div>
+    </>
+  );
+}
+
+export default AltHeader

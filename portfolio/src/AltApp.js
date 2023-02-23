0756f70635710141c8382ac6663278a1962e7761
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-23 10:38:28 -0500

    Create AltApp.js
    setting up an alternate App to use to different styling on a planned update to the portfolio

---
+++ portfolio/src/AltApp.js
@@ -0,0 +1,14 @@
+import AltAboutMe from "./AltComponents/AltAboutMe";
+import Contact from "./Components/Contact";
+
+
+function AltApp() {
+  return (
+    <>
+    <AltAboutMe /> 
+    <Contact />
+    </>
+  )
+}
+
+export default AltApp

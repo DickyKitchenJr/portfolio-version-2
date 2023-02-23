9a49823efc2337377592bc201bc4549852598bd3
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-23 10:39:37 -0500

    Update index.js
    temporarily commenting out App to see how changes look on AltApp prior to creating code to switch between the two

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AltApp from './AltApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AltApp />
   </React.StrictMode>
 );
 

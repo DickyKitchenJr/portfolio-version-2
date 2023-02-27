0a3572e0852bf07238ea456a4c6dbc3ef22190cf
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-27 14:03:24 -0500

    Create MyStyleApp.js

---
+++ portfolio/src/MyStyleApp.js
@@ -0,0 +1,32 @@
+import { Parallax } from "react-parallax";
+import code from "./Images/codeBackground.webp";
+import Header from "./Components/Header";
+import Skills from "./Components/Skills";
+import AboutMe from "./Components/AboutMe";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import "./App.css";
+
+function MyStyleApp() {
+  return (
+    <>
+      <Parallax
+        blur={0}
+        bgImage={code}
+        bgImageAlt="various lines of code"
+        contentClassName="main"
+        strength={600}
+      >
+        <Header />
+        <Skills />
+        <AboutMe />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Parallax>
+    </>
+  );
+}
+
+export default MyStyleApp;

ed02f2337077fb474d11cd2f3f47aae95f4355e9
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-27 14:03:35 -0500

    Create BonesLightApp.js

---
+++ portfolio/src/BonesLightApp.js
@@ -0,0 +1,22 @@
+import AltHeader from "./AltComponents/AltHeader";
+import AltAboutMe from "./AltComponents/AltAboutMe";
+import AltSkills from "./AltComponents/AltSkills";
+import AltProjects from "./AltComponents/AltProjects";
+import AltContact from "./AltComponents/AltContact";
+import AltFooter from "./AltComponents/AltFooter";
+
+
+function BonesLightApp() {
+  return (
+    <>
+    <AltHeader />
+    <AltAboutMe />
+    <AltSkills /> 
+    <AltProjects />
+    <AltContact />
+    <AltFooter />
+    </>
+  )
+}
+
+export default BonesLightApp

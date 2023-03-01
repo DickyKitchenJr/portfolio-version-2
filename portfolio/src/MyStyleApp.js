a41d10d559a19b8dd80fd8eb4c8de662f8ed372c
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-01 13:19:13 -0500

    Update MyStyleApp.js

--- portfolio/src/MyStyleApp.js
+++ portfolio/src/MyStyleApp.js
@@ -6,7 +6,7 @@ import AboutMe from "./Components/AboutMe";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
-import "./App.css";
+
 
 function MyStyleApp() {
   return (

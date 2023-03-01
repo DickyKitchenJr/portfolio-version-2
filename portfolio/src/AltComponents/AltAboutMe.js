209b3f070a0e1f2e82edcfd3d5c82c717758c3c9
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-01 13:23:10 -0500

    Update AltAboutMe.js

--- portfolio/src/AltComponents/AltAboutMe.js
+++ portfolio/src/AltComponents/AltAboutMe.js
@@ -1,50 +1,12 @@
-import React, { useEffect, useState } from "react";
-import meCentered from "../Images/meCenteredBW.webp";
-import meWide from "../Images/meWideBW.webp";
+import React from "react";
 
 function AltAboutMe() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  const [picture, setPicture] = useState();
-
-  const detectWindowSize = () => {
-    setWindowSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", detectWindowSize);
-    return () => {
-      window.removeEventListener("resize", detectWindowSize);
-    };
-  }, [windowSize]);
-
-  useEffect(() => {
-    const backgroundImage = () => {
-      if (windowSize.width <= 1020) {
-        setPicture(meCentered);
-      } else {
-        setPicture(meWide);
-      }
-    };
-
-    window.addEventListener("resize", backgroundImage());
-    return () => {
-      window.removeEventListener("resize", backgroundImage());
-    };
-  }, [windowSize]);
-
   return (
     <>
       <div className="altAboutMeTitle">
         <h2>About Me</h2>
       </div>
-      <div className="altAboutMe" style={{ backgroundImage: `url(${picture})` }}>
+      <div className="altAboutMe">
         <p className="altAboutMeContent">
           I'm a front end developer with the goal of concise, clean coding with
           user accessibility in mind.
@@ -63,7 +25,7 @@ function AltAboutMe() {
           <br />
           At first, I did most of my coding on the CodeCademy platform, but
           recently I have performed more coding in my GitHub repositories to
-          more easily show activity and growth. <br />
+          better let others see my activity and growth. <br />
           <br />
           Moving forward, my goal is to find a company I can grow at and a team
           that creates great things. <br />

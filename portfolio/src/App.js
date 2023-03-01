b9dd2dc6abb54c7a0d5b50758375a8341cd3ba3a
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-01 13:23:00 -0500

    Update App.js
    switched original app to MyStyleApp and changed App to house all versions which will then be handled by a user selecting a style once completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,30 +1,15 @@
-import { Parallax } from "react-parallax";
-import code from "./Images/codeBackground.webp";
-import Header from "./Components/Header";
-import Skills from "./Components/Skills";
-import AboutMe from "./Components/AboutMe";
-import Projects from "./Components/Projects";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
-import "./App.css";
+import MyStyleApp from "./MyStyleApp";
+import BonesLightApp from "./BonesLightApp";
+import BonesDarkApp from "./BonesDarkApp";
+import ModernMinimalistApp from "./ModernMinimalistApp";
 
 function App() {
   return (
     <>
-      <Parallax
-        blur={0}
-        bgImage={code}
-        bgImageAlt="various lines of code"
-        contentClassName="main"
-        strength={600}
-      >
-        <Header />
-        <Skills />
-        <AboutMe />
-        <Projects />
-        <Contact />
-        <Footer />
-      </Parallax>
+    <MyStyleApp />
+    <BonesLightApp />
+    <BonesDarkApp />
+    <ModernMinimalistApp />
     </>
   );
 }

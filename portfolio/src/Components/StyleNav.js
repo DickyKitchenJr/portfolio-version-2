88ce392df90bba6e975613faa6ef060c0a90a52a
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-01 13:18:58 -0500

    setting up UI selector section for choosing style

---
+++ portfolio/src/Components/StyleNav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './StyleNav.css';
+
+function StyleNav() {
+  return (
+    <>
+    <div className='style'>
+        <h2>Choose A Style</h2>
+    <nav>
+      <p>Creator's Pick (default)</p>
+      <p>Bare Bones Light Dark</p>
+      <p>Modern Minimalist Light Dark</p>
+    </nav>
+    </div>
+    </>
+    
+  )
+}
+
+export default StyleNav

b4d8d40b16631f035a8ee34832e635c2f2bda16b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-06 13:55:39 -0500

    Update OldApp.js

--- portfolio/src/OldApp.js
+++ portfolio/src/OldApp.js
@@ -1,15 +1,9 @@
 import MyStyleApp from "./MyStyleApp";
-import BonesLightApp from "./BonesLightApp";
-import BonesDarkApp from "./BonesDarkApp";
-import ModernMinimalistApp from "./ModernMinimalistApp";
 
 function OldApp() {
   return (
     <>
-    {/* <MyStyleApp /> */}
-    {/* <BonesLightApp /> */}
-    <BonesDarkApp />
-    {/* <ModernMinimalistApp /> */}
+    <MyStyleApp />
     </>
   );
 }

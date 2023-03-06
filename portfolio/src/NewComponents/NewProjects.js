193b0e84c4f4aeb3e6794d06fdf6ba4fbaffefa3
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-06 16:35:56 -0500

    Update NewProjects.js

--- portfolio/src/NewComponents/NewProjects.js
+++ portfolio/src/NewComponents/NewProjects.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import NewProjectContents from "./NewProjectContents";
 import { NewProjectEntries } from "./NewProjectEntries";
 
-function NewProjects({styling}) {
+function NewProjects({ styling }) {
   const [displayProject, setDisplayProject] = useState(0);
 
   const handleClickPrior = () => {
     if (displayProject === 0) {
-      setDisplayProject(NewProjectEntries.length -1);
+      setDisplayProject(NewProjectEntries.length - 1);
     } else {
       setDisplayProject(displayProject - 1);
     }
   };
 
   const handleClickNext = () => {
-    if (displayProject === NewProjectEntries.length -1) {
+    if (displayProject === NewProjectEntries.length - 1) {
       setDisplayProject(0);
     } else {
       setDisplayProject(displayProject + 1);
@@ -26,13 +26,19 @@ function NewProjects({styling}) {
       <div className={`${styling}-projectsTitle`}>
         <h2>Projects</h2>
         <button className={`${styling}-previous`} onClick={handleClickPrior}>
+          {styling === "modernLight" || styling === "modernDark" ? (
+            <span>◄</span>
+          ) : null}
           Prior
         </button>
         <button className={`${styling}-next`} onClick={handleClickNext}>
           Next
+          {styling === "modernLight" || styling === "modernDark" ? (
+            <span>►</span>
+          ) : null}
         </button>
       </div>
-      <NewProjectContents displayProject={displayProject} styling={styling}/>
+      <NewProjectContents displayProject={displayProject} styling={styling} />
     </>
   );
 }

0c832aac8444b94b9f80a58f945e568b8e412342
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-07 13:06:46 -0500

    Update ProjectContents.js

--- portfolio/src/Components/ProjectContents.js
+++ portfolio/src/Components/ProjectContents.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import "./Projects.css";
 import { ProjectEntries } from "./ProjectEntries";
 
-function ProjectContents({displayProject}) {
+function ProjectContents({ displayProject, styling }) {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -22,39 +21,49 @@ function ProjectContents({displayProject}) {
     };
   }, [windowSize]);
 
-
   return (
     <>
       {ProjectEntries.map((entry) => {
         return (
           <div
             key={ProjectEntries.indexOf(entry)}
-            style={{
-              backgroundImage:
-                windowSize.width >= 600
-                  ? `url(${entry.backgroundImage})`
-                  : `url(${entry.backgroundImageSmall})`,
-              display:
-                displayProject !== ProjectEntries.indexOf(entry)
-                  ? "none"
-                  : "block",
-            }}
-            className="projects"
+            style={
+              styling === "default"
+                ? {
+                    backgroundImage:
+                      windowSize.width >= 600
+                        ? `url(${entry.backgroundImage})`
+                        : `url(${entry.backgroundImageSmall})`,
+                    display:
+                      displayProject !== ProjectEntries.indexOf(entry)
+                        ? "none"
+                        : "block",
+                  }
+                : {
+                    display:
+                      displayProject !== ProjectEntries.indexOf(entry)
+                        ? "none"
+                        : "block",
+                  }
+            }
+            className={`${styling}-projects`}
           >
-            <div className="projectsContent">
+            <div className={`${styling}-projectsContent`}>
               <h3>{entry.Title}</h3>
               <p>Purpose:</p>
               <ul>
-                {entry.Pupose.map((reason) => {
+                {entry.Purpose.map((reason) => {
                   return <li>{reason}</li>;
                 })}
               </ul>
-              {entry.ClientReview ? (<>
-              <p>Client Review:</p>
-              <ul>
-                <li>{entry.ClientReview}</li>
-              </ul>
-              </>) : null}
+              {entry.ClientReview ? (
+                <>
+                  <p>Client Review:</p>
+                  <ul>
+                    <li>{entry.ClientReview}</li>
+                  </ul>
+                </>
+              ) : null}
               <p>Resources Used:</p>
               <ul>
                 {entry.Resources.map((used) => {
@@ -71,22 +80,26 @@ function ProjectContents({displayProject}) {
                 <img
                   src={entry.Test}
                   alt="score from page speed insight"
-                  className="pagespeed"
+                  className={`${styling}-pagespeed`}
                 />
               ) : null}
-              {entry.Improvements ? (<><p>Potential Future Improvements:</p>
-              <ul>
-                {entry.Improvements.map((improve) => {
-                  return <li>{improve}</li>;
-                })}
-              </ul></>): null}
+              {entry.Improvements ? (
+                <>
+                  <p>Potential Future Improvements:</p>
+                  <ul>
+                    {entry.Improvements.map((improve) => {
+                      return <li>{improve}</li>;
+                    })}
+                  </ul>
+                </>
+              ) : null}
               {entry.Source ? (
                 <>
                   <p>Source Code:</p>
                   <ul>
                     <li>
                       <a
-                        className="link"
+                        className={`${styling}-link`}
                         href={entry.Source}
                         target="_blank"
                         rel="noreferrer"
@@ -104,7 +117,7 @@ function ProjectContents({displayProject}) {
                   <ul>
                     <li>
                       <a
-                        className="link"
+                        className={`${styling}-link`}
                         href={entry.Live}
                         target="_blank"
                         rel="noreferrer"

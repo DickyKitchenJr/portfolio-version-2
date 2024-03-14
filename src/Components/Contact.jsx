d2764f3b01e489597401ae17e49acbd41eaac5d1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-14 13:50:32 -0400

    Update Contact.jsx
    updated contact form using Email.js to handle form submissions

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
-function Contact({styling}) {
+function Contact({ styling }) {
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [honey, setHoney] = useState(false)
+
+  const handleNameChange = (e) => {
+    let input = e.target.value;
+
+    setName(input);
+  };
+
+  const handleEmailChange = (e) => {
+    let input = e.target.value;
+
+    setEmail(input);
+  };
+
+  const handleMessageChange = (e) => {
+    let input = e.target.value;
+
+    setMessage(input);
+  };
+
+  const handleHoneyChange = () => {
+    setHoney(true)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("contact_service", "template_18nwxbn", form.current, {
+        publicKey: "iX7305v5_CCdTLK2r",
+      })
+      .then(
+        () => {
+          alert("Message Sent!");
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
+          alert(`Message failed. Error: ${error}`);
+          console.log(error);
+        }
+      );
+  };
+
   return (
     <>
       <div className={`${styling}-contactTitle`}>
@@ -8,24 +58,32 @@ function Contact({styling}) {
       </div>
       <div className={`${styling}-contactBackground`}>
         <div className={`${styling}-contact`}>
-          <form
-            action="https://formsubmit.co/dkitchencodes@gmail.com"
-            method="POST"
-          >
+          <form onSubmit={handleSubmit} ref={form}>
             <p>Have a job or a question for me?</p>
-            <input type="text" name="name" placeholder="Your Name" required />
             <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
+              type="text"
+              name="user_name"
+              placeholder="Your Name"
+              onChange={handleNameChange}
+              value={name}
               required
             />
-            <input type="text" name="_honey" style={{ display: "none" }} />
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Your Email"
+              onChange={handleEmailChange}
+              value={email}
+              required
+            />
+            <input type="checkbox" value={honey} onChange={handleHoneyChange} name="_honey" style={{ display: "none" }} />
             <textarea
               placeholder="Your Message"
               name="message"
               rows="10"
               cols="30"
+              onChange={handleMessageChange}
+              value={message}
               required
             ></textarea>
             <button type="submit">Send</button>

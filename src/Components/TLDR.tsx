69f004dda07a6ccc372705ba399527dfd637d4ed
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-06-06 13:57:28 -0400

    Create TLDR.tsx

---
+++ src/Components/TLDR.tsx
@@ -0,0 +1,17 @@
+function TLDR() {
+  return (
+    <>
+      <div className="style">
+        <h2>TL;DR</h2>
+        <p>
+          I am a software developer in Virginia Beach VA who specializes in
+          React.js and JavaScript/TypeScript development. In addition, I have
+          full stack experience using Node.js, Express.js, MySQL, and testing
+          frameworks including Jest and Vitest
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TLDR;

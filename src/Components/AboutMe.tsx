5e9073b530681649d9f4373431d1c29094f7b751
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-06-07 14:59:38 -0400

    Update AboutMe.tsx
    - removed unnecessary div
    - removed unused classNames

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -2,50 +2,48 @@ function AboutMe() {
   return (
     <>
       <h2>About Me</h2>
-      <div className="aboutMe">
-        <p className="aboutMeContent">
-          As a software developer, I bring a unique perspective to my work that
-          is informed by my experience as a Physical Therapist Assistant. Over
-          my 15-year career in healthcare (2 as a PT Tech and 13 as a LPTA), I
-          developed valuable skills in clear communication, attention to detail,
-          and a commitment to providing high-quality care. These skills have
-          translated seamlessly into my coding career, where I'm dedicated to
-          creating websites and applications that make a positive impact on
-          people's lives.
-          <br />
-          <br />
-          With a solid foundation in HTML, CSS, JavaScript, and React, I'm able
-          to approach every project with a sense of curiosity and excitement,
-          just as I approached every patient with a desire to help them achieve
-          their goals. In addition to my technical skills, I bring a range of
-          soft skills to my work, including effective verbal and written
-          communication, a team-oriented mindset, problem-solving ability,
-          creativity, and adaptability. I'm always eager to learn and improve,
-          and I value constructive feedback as an opportunity to grow and
-          develop my skills further.
-          <br />
-          <br />
-          Whether I'm working independently or collaborating with a team, I
-          approach every project with an open-minded, teachable attitude and a
-          commitment to delivering results that exceed expectations. I'm excited
-          about the opportunity to leverage my skills and experience to make a
-          positive impact in the software development space and to contribute to
-          innovative, user-centered solutions for clients and users alike.
-          <br />
-          <br />
-          (if you are interested in seeing a list of certificates I have earned,
-          you can click{" "}
-          <a className="link" href="https://certificates.dickykitchen.com/">
-            here
-          </a>
-          )
-          <br />
-          <br />
-          In addition to my years of experience in health care and time I've
-          taken to learn coding, I'm also a published author of 2 thriller books
-          and 1 children's book, as well as a loving husband and father.
-        </p>
-      </div>
+      <p>
+        As a software developer, I bring a unique perspective to my work that is
+        informed by my experience as a Physical Therapist Assistant. Over my
+        15-year career in healthcare (2 as a PT Tech and 13 as a LPTA), I
+        developed valuable skills in clear communication, attention to detail,
+        and a commitment to providing high-quality care. These skills have
+        translated seamlessly into my coding career, where I'm dedicated to
+        creating websites and applications that make a positive impact on
+        people's lives.
+        <br />
+        <br />
+        With a solid foundation in HTML, CSS, JavaScript, and React, I'm able to
+        approach every project with a sense of curiosity and excitement, just as
+        I approached every patient with a desire to help them achieve their
+        goals. In addition to my technical skills, I bring a range of soft
+        skills to my work, including effective verbal and written communication,
+        a team-oriented mindset, problem-solving ability, creativity, and
+        adaptability. I'm always eager to learn and improve, and I value
+        constructive feedback as an opportunity to grow and develop my skills
+        further.
+        <br />
+        <br />
+        Whether I'm working independently or collaborating with a team, I
+        approach every project with an open-minded, teachable attitude and a
+        commitment to delivering results that exceed expectations. I'm excited
+        about the opportunity to leverage my skills and experience to make a
+        positive impact in the software development space and to contribute to
+        innovative, user-centered solutions for clients and users alike.
+        <br />
+        <br />
+        (if you are interested in seeing a list of certificates I have earned,
+        you can click{" "}
+        <a className="link" href="https://certificates.dickykitchen.com/">
+          here
+        </a>
+        )
+        <br />
+        <br />
+        In addition to my years of experience in health care and time I've taken
+        to learn coding, I'm also a published author of 2 thriller books and 1
+        children's book, as well as a loving husband and father.
+      </p>
     </>
   );
 }

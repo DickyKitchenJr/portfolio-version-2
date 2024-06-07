5e5e4e18a6daac077d6120c6bfc184d1397ca70b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-06-07 16:04:36 -0400

    Update ProjectContents.tsx
    - added ProjectEntries into div for styling of entire section
    - removed 'Improvements' section

--- src/Components/ProjectContents.tsx
+++ src/Components/ProjectContents.tsx
@@ -7,102 +7,86 @@ interface ProjectContentsProps {
 function ProjectContents({ displayProject }: ProjectContentsProps) {
   return (
     <>
-      {ProjectEntries.map((entry) => {
-        return (
-          <div
-            key={ProjectEntries.indexOf(entry)}
-            style={{
-              display:
-                displayProject !== ProjectEntries.indexOf(entry)
-                  ? "none"
-                  : "block",
-            }}
-            className="projects"
-          >
-            <div className="projectsContent">
-              <h3>{entry.Title}</h3>
-              <p>Purpose:</p>
-              <ul>
-                {entry.Purpose.map((reason) => {
-                  return <li>{reason}</li>;
-                })}
-              </ul>
-              {entry.ClientReview ? (
-                <>
-                  <p>Client Review:</p>
-                  <ul>
-                    <li>{entry.ClientReview}</li>
-                  </ul>
-                </>
-              ) : null}
-              <p>Resources Used:</p>
-              <ul>
-                {entry.Resources.map((used) => {
-                  return <li>{used}</li>;
-                })}
-              </ul>
-              <p>Achievements:</p>
-              <ul>
-                {entry.Achievements.map((achievement) => {
-                  return <li>{achievement}</li>;
-                })}
-              </ul>
-              {entry.Test ? (
-                <img
-                  src={entry.Test}
-                  alt="score from page speed insight"
-                  className="pagespeed"
-                />
-              ) : null}
-              {entry.Improvements ? (
-                <>
-                  <p>Potential Future Improvements:</p>
-                  <ul>
-                    {entry.Improvements.map((improve) => {
-                      return <li>{improve}</li>;
-                    })}
-                  </ul>
-                </>
-              ) : null}
-              {entry.Source ? (
-                <>
-                  <p>Source Code:</p>
-                  <ul>
-                    <li>
-                      <a
-                        className="link"
-                        href={entry.Source}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        GitHub
-                      </a>
-                    </li>
-                  </ul>
-                </>
-              ) : null}
-
-              {entry.Live ? (
-                <>
-                  <p>Live Site:</p>
-                  <ul>
-                    <li>
-                      <a
-                        className="link"
-                        href={entry.Live}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Hosted Here
-                      </a>
-                    </li>
-                  </ul>
-                </>
-              ) : null}
+      <div className="project">
+        {ProjectEntries.map((entry) => {
+          return (
+            <div
+              key={ProjectEntries.indexOf(entry)}
+              style={{
+                display:
+                  displayProject !== ProjectEntries.indexOf(entry)
+                    ? "none"
+                    : "block",
+              }}
+              className="projects"
+            >
+              <div className="projectsContent">
+                <h3>{entry.Title}</h3>
+                <h4>Purpose:</h4>
+                <ul>
+                  {entry.Purpose.map((reason) => {
+                    return <li>{reason}</li>;
+                  })}
+                </ul>
+                {entry.ClientReview ? (
+                  <>
+                    <h4>Client Review:</h4>
+                    <ul>
+                      <li>{entry.ClientReview}</li>
+                    </ul>
+                  </>
+                ) : null}
+                <h4>Resources Used:</h4>
+                <ul>
+                  {entry.Resources.map((used) => {
+                    return <li>{used}</li>;
+                  })}
+                </ul>
+                <h4>Achievements:</h4>
+                <ul>
+                  {entry.Achievements.map((achievement) => {
+                    return <li>{achievement}</li>;
+                  })}
+                </ul>
+                {entry.Source ? (
+                  <>
+                    <h4>Source Code:</h4>
+                    <ul>
+                      <li>
+                        <a
+                          className="link"
+                          href={entry.Source}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          GitHub
+                        </a>
+                      </li>
+                    </ul>
+                  </>
+                ) : null}
+                {entry.Live ? (
+                  <>
+                    <h4>Live Site:</h4>
+                    <ul>
+                      <li>
+                        <a
+                          className="link"
+                          href={entry.Live}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Hosted Here
+                        </a>
+                      </li>
+                    </ul>
+                  </>
+                ) : null}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 }
